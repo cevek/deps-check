fd9615df6af83f92281619f2552c094292380800
cody <cevek@ya.ru>
2019-03-20 01:25:12 +0500

    Refactor to auto check root package

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
 import {dirname} from 'path';
+import {existsSync} from 'fs';
 
-export default function(packageJSONFile: string) {
-    const projectDir = dirname(packageJSONFile);
-    const json = require(packageJSONFile);
-    const semver = require('semver');
-    const deps = {...json.dependencies, ...json.devDependencies};
-    for (const pkg in deps) {
-        const requiredVersion = deps[pkg];
-        const currentVersion = require(projectDir + '/node_modules/' + pkg + '/package.json').version;
-        if (/#/.test(currentVersion)) continue;
-        if (requiredVersion === 'latest') continue;
-        if (!semver.satisfies(currentVersion, requiredVersion)) {
-            throw new Error(`${pkg}: installed: ${currentVersion}, expected: ${requiredVersion}`);
-        }
+let projectDir = '';
+for (let i = 0; i < require.main!.paths.length; i++) {
+    const path = dirname(require.main!.paths[i]);
+    if (existsSync(path + '/package.json')) {
+        projectDir = path;
+        break;
     }
 }
+if (projectDir === '') throw new Error('root package.json is not found');
+const json = require(projectDir + '/package.json');
+const semver = require('semver');
+const deps = {...json.dependencies, ...json.devDependencies};
+for (const pkg in deps) {
+    const requiredVersion = deps[pkg];
+    const currentVersion = require(projectDir + '/node_modules/' + pkg + '/package.json').version;
+    if (/#/.test(currentVersion)) continue;
+    if (requiredVersion === 'latest') continue;
+    if (!semver.satisfies(currentVersion, requiredVersion)) {
+        throw new Error(`${pkg}: installed: ${currentVersion}, expected: ${requiredVersion}`);
+    }
+}
+
+export {}
